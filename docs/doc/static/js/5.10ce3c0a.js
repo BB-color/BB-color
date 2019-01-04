38883d3f93a5a65fbf22c32f51eb588cedd012ca
ParaSLee <610033941@qq.com>
2019-01-04 21:42:09 +0800

    '测试能否跳转'

---
+++ docs/doc/static/js/5.10ce3c0a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"./.docz/app/imports.js":function(n,o,t){"use strict";t.r(o),t.d(o,"imports",function(){return c});var c={"src/components/home.mdx":function(){return t.e(3).then(t.bind(null,"./src/components/home.mdx"))},"src/components/button/button.mdx":function(){return t.e(2).then(t.bind(null,"./src/components/button/button.mdx"))}}}},0,[2,3]]);
+//# sourceMappingURL=5.6ba91fd0ce4caec31588.js.map
---
+++ doczrc.js
@@ -0,0 +1,12 @@
+// doczrc.js
+import { css } from 'docz-plugin-css'
+
+export default {
+  title: 'BB Color',
+  description: 'A Design UI library for React',
+  plugins: [
+    css({
+      preprocessor: 'postcss'
+    })
+  ]
+}
